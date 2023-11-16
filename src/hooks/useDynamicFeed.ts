2a6bcce41f3e25888574c357b9df1a06673fec2b
Fatpandac <i@fatpandac.com>
2023-11-16 11:08:52 +0800

    fix: add new cache to record watched videos

--- src/hooks/useDynamicFeed.ts
+++ src/hooks/useDynamicFeed.ts
@@ -2,11 +2,13 @@ import { getDynamicFeed, getPlayUrl, getVideoInfo } from "../utils";
 
 import { useEffect, useState } from "react";
 import { showToast, Toast } from "@raycast/api";
+import { useCachedState } from "@raycast/utils";
 
 export function useDynamicFeed() {
   const [dynamicItems, setDynamicItems] = useState<Bilibili.DynamicItems>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [shouldRefetch, refetch] = useState({});
+  const [watchedList, setWatchedList] = useCachedState<Array<string>>("watchedList", []);
 
   useEffect(() => {
     (async () => {
@@ -17,10 +19,17 @@ export function useDynamicFeed() {
           res.map(async (item: Bilibili.DynamicItem) => {
             if (item.type !== "DYNAMIC_TYPE_AV") return item;
 
-            const videoInfo = await getVideoInfo(item.modules.module_dynamic.major.archive.aid);
+            const { aid } = item.modules.module_dynamic.major.archive;
+            const videoInfo = await getVideoInfo(aid);
             const videoPlayUrl = await getPlayUrl(videoInfo.bvid, videoInfo.cid.toString());
 
-            item.modules.module_dynamic.major.archive.last_play_time = videoPlayUrl.last_play_time;
+            // save played video aid into watchedList
+            if (videoPlayUrl.last_play_time) {
+              setWatchedList([aid, ...watchedList].slice(0, 200));
+            }
+
+            item.modules.module_dynamic.major.archive.last_play_time =
+              videoPlayUrl.last_play_time || +watchedList.includes(aid);
 
             return item;
           })
@@ -29,7 +38,6 @@ export function useDynamicFeed() {
         setDynamicItems(dynamicList);
         setIsLoading(false);
       } catch (error) {
-        console.log(error);
         showToast(Toast.Style.Failure, "Get dynamic video feed failed");
       }
     })();
